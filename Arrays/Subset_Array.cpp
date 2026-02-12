1eca20a0f0dccf9e7582762dbbef83803c0171bf
Rama-chandra55 <rock6262712@gmail.com>
2026-02-12 22:18:34 +0530

    Frequency counting using Hashing

---
+++ Arrays/Subset_Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool isSubset(vector<int> &a, vector<int> &b) {
+        unordered_map<int, int> freq;
+        for(int x : a){
+            freq[x]++;
+        }
+        for(int x : b){
+            if(freq[x] == 0){
+                return false;
+            }
+            freq[x]--;
+        }
+        return true;
+    }
+};
